cab5a7d3027b42562c6cf648df5472a82f57453c
Danitha Dinuwan <danithahk@gmail.com>
2026-03-28 10:11:35 +0530

    update

---
+++ src/main/java/com/cosmic/user/UserController.java
@@ -0,0 +1,34 @@
+package com.cosmic.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    // Additional endpoints for user login
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        // Implement user login logic
+
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+    }
+    
+}
